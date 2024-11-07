73180513c804c4735e540cae9ca3b7195f115e6b
RenpuLiu <66483750+RenpuLiu@users.noreply.github.com>
2024-11-07 13:19:08 -0500

    Update train.py

--- src/train.py
+++ src/train.py
@@ -23,7 +23,7 @@ def train_step(model, xs, ys, head_mask, optimizer, loss_func):
     optimizer.zero_grad()
     output = model(xs, ys, head_mask)
     if 'SoftmaxEncoder' in model.name:
-        loss = loss_func(output, xs)
+        loss = loss_func(output[:,5:,:], xs[:,5:,:])
     else:
         loss = loss_func(output, ys)
     loss.backward()
