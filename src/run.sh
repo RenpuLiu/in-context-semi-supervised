a53c7e29b296ec9f3eab846d2de9925646aa9786
RenpuLiu <66483750+RenpuLiu@users.noreply.github.com>
2025-05-11 01:01:20 +0800

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,6 @@
-UNLAB_LIST = (0 1 2 3 4 5 6 7 8 9 10)
+conda activate in-context-learning
+
+UNLAB_LIST=(0 1 2 3 4 5 6 7 8 9 10)
 
 for M in "${UNLAB_LIST[@]}"; do
   python train.py --config conf/semi_supervised.yaml
