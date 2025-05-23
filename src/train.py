a40d94bae0ac3ca49435d568c0dd3bba09487163
RenpuLiu <66483750+RenpuLiu@users.noreply.github.com>
2025-05-23 15:59:35 +0800

    Update train.py

--- src/train.py
+++ src/train.py
@@ -289,7 +289,7 @@ def train(model, args):
             **data_sampler_args,
         )
         xs_val = data_sampler.sample_xs(
-            15,
+            100,
             bsize,
             curriculum.n_dims_truncated,
             **data_sampler_args,
